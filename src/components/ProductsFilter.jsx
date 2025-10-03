9647ee96b666e15ecc59bd893b1b7d5bc90233c9
Burak3745 <69092099+Burak3745@users.noreply.github.com>
2025-10-04 02:15:32 +0300

    final project

--- src/components/ProductsFilter.jsx
+++ src/components/ProductsFilter.jsx
@@ -3,7 +3,7 @@ import "../css/ProductsFilter.css";
 const ProductsFilter = ({ onFilterChange, selectedPriceRange, setSelectedPriceRange, selectedPopularity, setSelectedPopularity }) => {
 
     const priceRanges = [
-        { label: "Tüm Fiyatlar", value: "" },
+        { label: "All Prices", value: "" },
         { label: "0 - 500 USD", value: "0-500" },
         { label: "500 - 1000 USD", value: "500-1000" },
         { label: "1000 - 2000 USD", value: "1000-2000" },
@@ -11,12 +11,12 @@ const ProductsFilter = ({ onFilterChange, selectedPriceRange, setSelectedPriceRa
     ];
 
     const popularityOptions = [
-        { label: "Tüm Popülerlik", value: "" },
-        { label: "Çok İyi", value: "0.8-1", stars: 5 },
-        { label: "İyi", value: "0.6-0.8", stars: 4 },
-        { label: "Ne İyi / Ne Kötü", value: "0.4-0.6", stars: 3 },
-        { label: "Kötü", value: "0.2-0.4", stars: 2 },
-        { label: "Çok Kötü", value: "0-0.2", stars: 1 },
+        { label: "All Popularity", value: "" },
+        { label: "Excellent", value: "0.8-1", stars: 5 },
+        { label: "Good", value: "0.6-0.8", stars: 4 },
+        { label: "Average", value: "0.4-0.6", stars: 3 },
+        { label: "Poor", value: "0.2-0.4", stars: 2 },
+        { label: "Very Poor", value: "0-0.2", stars: 1 },
     ];
 
     const handlePriceChange = (e) => {
@@ -48,7 +48,7 @@ const ProductsFilter = ({ onFilterChange, selectedPriceRange, setSelectedPriceRa
             <select onChange={handlePopularityChange} value={selectedPopularity}>
                 {popularityOptions.map((opt, idx) => (
                     <option key={idx} value={opt.value}>
-                        {opt.stars ? `${opt.label} / ${"⭐"} ${opt.stars-1} - ${opt.stars}` : opt.label} 
+                        {opt.stars ? `${opt.label} / ${"⭐"} ${opt.stars - 1} - ${opt.stars}` : opt.label}
                     </option>
                 ))}
             </select>
